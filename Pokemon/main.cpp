388991911b711107fb378196f65d6a1c1217c6c9
Matheus Veras <mveras1897@gmail.com>
2025-08-05 18:41:45 -0300

    Create Main Function assignment

--- Pokemon/main.cpp
+++ Pokemon/main.cpp
@@ -4,9 +4,14 @@ using namespace std;
 int main() {
 
     string player_name;
-    cout << "Enter your name:" << endl;
+    cout << "Professor Oak: Trainer! Before you embark on your journey \n the Pokémon wolrd need to know who you are!";
+    cout << "Time to step up and tell us your name." << endl;
+    cout << "And remember, a great name is the start of every great adventure!" << endl;
+
+    cout << "What's your name?" << endl;
     cin >> player_name;
-    cout << "Great Start" << player_name << ", looks like you have understood the main function properly now" << endl;
+    cout << "Congratulations" << player_name << ", PokeID created!" << endl;
+    cout << "Let's gotcha them all!" << endl;
 
     return 0;
 }
